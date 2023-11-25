ac9a6fa76dee7d813c2838fedacfc5079935ca95
G_oaT <solarcrash3@gmail.com>
2023-11-25 18:22:59 +0300

    post

--- only_flask/app.py
+++ only_flask/app.py
@@ -5,7 +5,14 @@ app=Flask(__name__) # LEts  flask know where to look for static files
 # app Decorator(Determines the path inside the url patterns)
 @app.route('/'methods=['GET','POST'])
 def index():
-    return  'Greetings Developer!'
+   if(request.method =='GET'):
+       data="Amen Brother :)"
+       return jsonify({'A Message': data})
+
+@app.route('/<int:num>',methods=['GET'])
+def cube(num):
+    return jsonify({f'cube number of {num}':num**3})
+
 
 if __name__ =="__main__":
     app.run(debug=True)# Lets ou not have to restart the server anytime you make changes
